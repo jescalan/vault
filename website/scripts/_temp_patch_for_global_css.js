816db2d5b9dd38baf373a9070fab18b4f512e4dc
Jeff Escalante <jescalan@users.noreply.github.com>
2019-12-03 23:39:28 -0500

    implement postcss config

--- website/scripts/_temp_patch_for_global_css.js
+++ website/scripts/_temp_patch_for_global_css.js
@@ -2,15 +2,6 @@
 const fs = require('fs')
 const path = require('path')
 
-// rewrite webpack config to match what our custom postcss config
-fs.writeFileSync(
-  path.join(__dirname, '../node_modules/next/dist/build/webpack-config.js'),
-  fs.readFileSync(
-    path.join(__dirname, '_temp_nextjs_webpack_config_patch.txt'),
-    'utf8'
-  )
-)
-
 // rewrite nextjs-scripts config to remove next-css
 fs.writeFileSync(
   path.join(__dirname, '../node_modules/@hashicorp/nextjs-scripts/index.js'),
