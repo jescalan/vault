44b70bd865dee1efabc9a844e8cbf19d414a3321
Kevin Pruett <pruett.kevin@gmail.com>
2019-12-04 18:25:54 -0500

    Homepage content updates
    * Pulls content from updated Dato model and renders to homepage
    * Replicate DatoCMS changes to ProductSubNav via "mocked" function calls

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -14,6 +14,42 @@ import bugsnagClient from '../lib/bugsnag'
 import Error from './_error'
 import Link from 'next/link'
 
+/*
+ * We are capturing certain actions to be performed in DatoCMS prior to publish.
+ * For now, we can mock these out with simple function calls
+ * as to not cause current production issues
+ * Each change is appended with `- [ ]` (checkmark) task
+ */
+const _pipe = (a, b) => arg => b(a(arg))
+const pipe = (...fns) => fns.reduce(_pipe)
+
+const renameAndUpdateOverviewLink = data => {
+  // - [ ] change first link title from 'Overview' to 'Intro'
+  data.tdmFocusedLinks[0].title = 'Intro'
+  // - [ ] change first link url to '/intro'
+  data.tdmFocusedLinks[0].url = '/intro'
+
+  return data
+}
+
+const updateUseCases = data => {
+  // - [ ] add Identity-based Access use case
+  data.tdmFocusedLinks[1].links[2] = {
+    external: false,
+    title: 'Identity-based Access',
+    url: '/use-cases/identity-based-access'
+  }
+  // - [ ] update Secrets Management use case url
+  data.tdmFocusedLinks[1].links[0].url = '/use-cases/secrets-management'
+  // - [ ] update Data Encryption use case url
+  data.tdmFocusedLinks[1].links[1].url = '/use-cases/data-encryption'
+
+  return data
+}
+
+const __mockCmsChanges = pipe(renameAndUpdateOverviewLink, updateUseCases)
+/* End of mocked DatoCMS-related actions */
+
 Router.events.on('routeChangeStart', NProgress.start)
 Router.events.on('routeChangeError', NProgress.done)
 Router.events.on('routeChangeComplete', url => {
@@ -62,7 +98,8 @@ class NextApp extends App {
         <ProductSubnav
           Link={Link}
           dark={false}
-          subnav={globalData.productSubnav}
+          /* Remove call to __mockCmsChanges before production release */
+          subnav={__mockCmsChanges(globalData.productSubnav)}
           products={globalData.allEnterpriseProducts}
           currentPath={path}
           rootUrl="vaultproject.io"
