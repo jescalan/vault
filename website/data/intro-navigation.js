4640d6201b5ba838866ef5617a223576e85b2525
Kevin Pruett <pruett.kevin@gmail.com>
2019-12-05 19:57:34 -0500

    Intro layout and routing

---
+++ website/data/intro-navigation.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    category: 'getting-started',
+    content: [
+      'dev-server',
+      'first-secret',
+      'secrets-engines',
+      'dynamic-secrets',
+      'help',
+      'authentication',
+      'policies',
+      'deploy',
+      'apis',
+      'next-steps'
+    ]
+  }
+]
