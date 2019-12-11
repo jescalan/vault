fe5dcc1101df0f6f856145dba6755a03b193a0c2
Jeff Escalante <jescalan@users.noreply.github.com>
2019-12-11 17:11:56 -0500

    mock out local subnav data

---
+++ website/data/subnav.js
@@ -0,0 +1,28 @@
+export default {
+  tdmFocusedLinks: [
+    { title: 'Overview', url: '/intro' },
+    {
+      title: 'Use Cases',
+      links: [
+        { title: 'Secrets Management', url: '/secrets-management' },
+        { title: 'Data Encryption', url: '/data-encryption' },
+        { title: 'Identity-based Access', url: '/identity-based-access' }
+      ]
+    },
+    {
+      title: 'Enterprise',
+      url: 'https://www.hashicorp.com/products/vault/enterprise'
+    },
+    {
+      title: 'Whitepaper',
+      url:
+        'https://www.hashicorp.com/resources/unlocking-the-cloud-operating-model-security?utm_source=vaultsubnav'
+    }
+  ],
+  practitionerFocusedLinks: [
+    { title: 'Learn', url: 'https://learn.hashicorp.com/vault' },
+    { title: 'Docs', url: '/docs' },
+    { title: 'API', url: '/api-docs' },
+    { title: 'Community', url: '/community' }
+  ]
+}
