c175a33264bb609f5db9c22eb5222bee986e7e65
Jeff Escalante <jescalan@users.noreply.github.com>
2019-12-12 14:10:54 -0500

    local-ize product subnav

--- website/data/subnav.js
+++ website/data/subnav.js
@@ -1,28 +1,25 @@
-export default {
-  tdmFocusedLinks: [
-    { title: 'Overview', url: '/intro' },
-    {
-      title: 'Use Cases',
-      links: [
-        { title: 'Secrets Management', url: '/secrets-management' },
-        { title: 'Data Encryption', url: '/data-encryption' },
-        { title: 'Identity-based Access', url: '/identity-based-access' }
-      ]
-    },
-    {
-      title: 'Enterprise',
-      url: 'https://www.hashicorp.com/products/vault/enterprise'
-    },
-    {
-      title: 'Whitepaper',
-      url:
-        'https://www.hashicorp.com/resources/unlocking-the-cloud-operating-model-security?utm_source=vaultsubnav'
-    }
-  ],
-  practitionerFocusedLinks: [
-    { title: 'Learn', url: 'https://learn.hashicorp.com/vault' },
-    { title: 'Docs', url: '/docs' },
-    { title: 'API', url: '/api-docs' },
-    { title: 'Community', url: '/community' }
-  ]
-}
+export default [
+  { title: 'Overview', url: '/intro' },
+  {
+    title: 'Use Cases',
+    links: [
+      { title: 'Secrets Management', url: '/secrets-management' },
+      { title: 'Data Encryption', url: '/data-encryption' },
+      { title: 'Identity-based Access', url: '/identity-based-access' }
+    ]
+  },
+  {
+    title: 'Enterprise',
+    url: 'https://www.hashicorp.com/products/vault/enterprise'
+  },
+  {
+    title: 'Whitepaper',
+    url:
+      'https://www.hashicorp.com/resources/unlocking-the-cloud-operating-model-security?utm_source=vaultsubnav'
+  },
+  { title: '|' },
+  { title: 'Learn', url: 'https://learn.hashicorp.com/vault' },
+  { title: 'Docs', url: '/docs' },
+  { title: 'API', url: '/api-docs' },
+  { title: 'Community', url: '/community' }
+]
--- website/pages/_app.js
+++ website/pages/_app.js
@@ -3,61 +3,22 @@ import App from 'next/app'
 import NProgress from 'nprogress'
 import Router from 'next/router'
 import DefaultHeadTags from '../components/default-head-tags'
+import ProductSubnav from '../components/subnav'
 import MegaNav from '@hashicorp/react-mega-nav'
 import Footer from '@hashicorp/react-footer'
-import ProductSubnav from '@hashicorp/react-product-subnav'
 import { ConsentManager } from '@hashicorp/react-consent-manager'
 import consentManagerConfig from '../lib/consent-manager-config'
 import { fetch } from '@hashicorp/nextjs-scripts/dato/client'
 import globalDataQuery from './globalData.graphql'
 import bugsnagClient from '../lib/bugsnag'
 import Error from './_error'
-import Link from 'next/link'
+import subnavLinks from '../data/subnav'
 import dynamic from 'next/dynamic'
 
 dynamic(
   () => document.body.msMatchesSelector && import('ie11-custom-properties'),
   { ssr: false }
 )
-
-/*
- * We are capturing certain actions to be performed in DatoCMS prior to publish.
- * For now, we can mock these out with simple function calls
- * as to not cause current production issues
- * However, these will need be performed within the CMS manually,
- * along with publishing this code to production
- *
- * Each change is appended with `- [ ]` (checkmark) task
- */
-const _pipe = (a, b) => arg => b(a(arg))
-const pipe = (...fns) => fns.reduce(_pipe)
-
-const renameAndUpdateOverviewLink = data => {
-  // - [ ] change first link title from 'Overview' to 'Intro'
-  data.tdmFocusedLinks[0].title = 'Intro'
-  // - [ ] change first link url to '/intro'
-  data.tdmFocusedLinks[0].url = '/intro'
-
-  return data
-}
-
-const updateUseCases = data => {
-  // - [ ] add Identity-based Access use case
-  data.tdmFocusedLinks[1].links[2] = {
-    external: false,
-    title: 'Identity-based Access',
-    url: '/use-cases/identity-based-access'
-  }
-  // - [ ] update Secrets Management use case url
-  data.tdmFocusedLinks[1].links[0].url = '/use-cases/secrets-management'
-  // - [ ] update Data Encryption use case url
-  data.tdmFocusedLinks[1].links[1].url = '/use-cases/data-encryption'
-
-  return data
-}
-
-const __mockCmsChanges = pipe(renameAndUpdateOverviewLink, updateUseCases)
-/* End of mocked DatoCMS-related actions */
 
 Router.events.on('routeChangeStart', NProgress.start)
 Router.events.on('routeChangeError', NProgress.done)
@@ -85,8 +46,7 @@ class NextApp extends App {
 
     const globalData = await fetch({
       query: globalDataQuery,
-      dependencies: [ProductSubnav, MegaNav],
-      variables: { productId: '85509' } // product id for vault in dato
+      dependencies: [MegaNav]
     })
 
     return { pageProps, globalData, path: ctx.asPath }
@@ -105,11 +65,7 @@ class NextApp extends App {
           titleUrl="https://vaultproject.io"
         />
         <ProductSubnav
-          Link={Link}
-          dark={false}
-          /* Remove call to __mockCmsChanges before production release */
-          subnav={__mockCmsChanges(globalData.productSubnav)}
-          products={globalData.allEnterpriseProducts}
+          links={subnavLinks}
           currentPath={path}
           rootUrl="vaultproject.io"
         />
